def489924eb7f4f75a7d9adaa168622e596a7ce0
Thomas Brookshaw <tbrookshaw13@gmail.com>
2020-10-22 20:52:06 -0500

    added imports, get axios, onmount function

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Header from '../Header/Header.js';
-import Header from '../GalleryList/GalleryList.js';
-import Header from '../GalleryItem/GalleryItem.js';
+import GalleryList from '../GalleryList/GalleryList.js';
+import GalleryItem from '../GalleryItem/GalleryItem.js';
 import './App.css';
 
 class App extends Component {
+  state = {
+    picsArray: []
+  }
+
+  componentDidMount = () => {
+    console.log('app.js is mounted')
+    this.getPics();
+  }
+
+  getPics = () => {
+    console.log('in getPics');
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    }).then((response) => {
+      console.log(response.data);
+      this.setState({
+        picsArray: response.data
+      });
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+
   render() {
     return (
       <div className="App">
        <Header />
         <p>Gallery goes here</p>
         <img src="images/goat_small.jpg"/>
+      
       </div>
     );
   }
